cb32aa392b07ef9671679a14c294c56f82461b5a
Volodymyr Dvorskyi <feowurd@gmail.com>
2021-09-18 15:33:27 +0300

    сделана разметка

--- src/index.js
+++ src/index.js
@@ -1,1 +1,24 @@
+import menu from './menu.json';
 import './sass/main.scss';
+import templateFunction from './template.hbs';
+
+
+const menuContainer = document.querySelector('.js-menu');
+const menuMarkup = createMenuMarkup(menu);
+const themeSwitcher = document.querySelector('#theme-switch-toggle');
+const bodyTheme = document.querySelector('body');
+const Theme = {
+  LIGHT: 'light-theme',
+  DARK: 'dark-theme',
+};
+
+menuContainer.insertAdjacentHTML('afterbegin', menuMarkup);
+bodyTheme.classList.add('light-theme')
+function createMenuMarkup(menu) {
+    return menu.map(templateFunction).join('')
+};
+
+themeSwitcher.addEventListener('change', themeToggle);
+function themeToggle (params) {
+    
+};
