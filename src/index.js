901e85c6b0cdfa13261bf14e30f15f8555f2510d
Volodymyr Dvorskyi <feowurd@gmail.com>
2021-09-29 18:44:30 +0300

    correct body theme

--- src/index.js
+++ src/index.js
@@ -15,7 +15,8 @@ const Theme = {
 menuContainer.insertAdjacentHTML('afterbegin', menuMarkup);
 
 
-localStorage.setItem('theme', Theme.LIGHT)
+localStorage.setItem('theme', Theme.LIGHT);
+bodyTheme.classList.add(localStorage.getItem('theme'));
 
 function createMenuMarkup(menu) {
     return menu.map(templateFunction).join('')
